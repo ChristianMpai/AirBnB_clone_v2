2b52e2e4b6db0d58c772058343ab0176070d20ee
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-12-08 05:10:07 +0200

    user class

--- models/place.py
+++ models/place.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-"""This is the place class."""
+"""This is the user class."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy import Table, Float, MetaData
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.amenity import Amenity
-from models.review import Review
-from sqlalchemy.ext.declarative import declarative_base
-import models
-
-metadata = Base.metadata
 
 
-class Place(BaseModel, Base):
-    """This is the class for Place
+class User(BaseModel, Base):
+    """This is the class for user
     Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
-        amenity_ids: list of Amenity ids
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = relationship('Amenity', secondary='place_amenity',
-                               viewonly=False)
-
-    place_amenity = Table('place_amenity', metadata,
-                          Column('place_id',
-                                 String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id',
-                                 String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True, nullable=False))
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    reviews = relationship('Review', backref='user',
+                           cascade='delete')
+    places = relationship('Place', backref='user',
+                          cascade='delete')
