c47681434a7d64dde07727b1a4afdbffb5fb4699
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-12-08 05:07:55 +0200

    places class

--- models/place.py
+++ models/place.py
@@ -1,79 +1,53 @@
 #!/usr/bin/python3
-""" holds class Place"""
+"""This is the place class."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy import Float
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Table, Float, MetaData
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
 import models
 
-if models.storage_type == "db":
-    class PlaceAmenity(Base):
-        __tablename__ = "place_amenity"
-        place_id = Column(String(80),
-                          ForeignKey("places.id"),
-                          primary_key=True,
-                          nullable=False)
-        amenity_id = Column(String(80),
-                            ForeignKey("amenities.id"),
-                            primary_key=True,
-                            nullable=False)
-        metadata = Base.metadata
+metadata = Base.metadata
 
 
 class Place(BaseModel, Base):
-    """Representation of Place """
-    if models.storage_type == "db":
-        __tablename__ = "places"
+    """This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
 
-        city_id = Column(String(60),
-                         ForeignKey("cities.id"))
-        user_id = Column(String(60),
-                         ForeignKey("users.id"))
-        name = Column(String(128),
-                      nullable=False)
-        description = Column(String(1024),
-                             nullable=True)
-        number_rooms = Column(Integer,
-                              nullable=False,
-                              default=0)
-        number_bathrooms = Column(Integer,
-                                  nullable=False,
-                                  default=0)
-        max_guest = Column(Integer,
-                           nullable=False,
-                           default=0)
-        price_by_night = Column(Integer,
-                                nullable=False,
-                                default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
-        reviews = relationship("Review",
-                               backref="place",
-                               cascade="delete")
-        amenities = relationship("Amenity",
-                                 secondary="place_amenity",
-                                 viewonly=False)
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = relationship('Amenity', secondary='place_amenity',
+                               viewonly=False)
 
-        @property
-        def reviews(self):
-            """Returns respective list of reviews"""
-            all_reviews = models.storage.all(Review)
-            return list(filter((lambda c: c.place_id == self.id), all_reviews))
-
-        @property
-        def amenities(self):
-            """Returns respective list of reviews"""
-            amenities = models.storage.all(Amenity)
-            return list(filter((lambda c: c.place_id == self.id), amenities))
-
-    def __init__(self, *args, **kwargs):
-        """initializes Place"""
-        super().__init__(*args, **kwargs)
+    place_amenity = Table('place_amenity', metadata,
+                          Column('place_id',
+                                 String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
